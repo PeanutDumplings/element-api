408ac9b3916a8a45979c4362743cf0b0bfbb960c
PeanutDumplings <nicholas.whyte@outlook.com>
2024-07-12 21:42:44 +0930

    convert processed json to csv

---
+++ data/jsonToCSV.ts
@@ -0,0 +1,18 @@
+import { json2csv } from "json-2-csv";
+import processed_data from "./output/processed_data.json";
+import fs from "fs";
+import path from "path";
+
+const csv = json2csv(processed_data);
+// console.log(csv);
+
+const outputDir = "./data/output";
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+const outputFile = path.join(outputDir, "processed_data.csv");
+fs.writeFileSync(outputFile, csv);
+
+console.log(`Data written to ${outputFile}`);
